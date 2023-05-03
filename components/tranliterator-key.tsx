e23786548a70a2b0a85bc6e8c7c16520cfcada14
order&chaos <sean@orderandchaoscreative.com>
2023-05-03 08:40:47 +0100

    Update key

--- components/tranliterator-key.tsx
+++ components/tranliterator-key.tsx
@@ -3,18 +3,17 @@ const TransliteratorKey = () =>
         <h3 className='font-bold'>Key</h3>
         <ul>
             <li>
+                Non-highlighted text is a straight look up from an IPA dictionary for the chosen language
+            </li>
+            <li>
                 <span className='text-blue-400'>Blue</span> highlighted text has alternate pronunciations,
                 click on the text to view and select them.
             </li>
             <li>
-                <span className='text-red-400'>Red</span> highlighted text has no dictionary look up and may
-                have been transliterated using phoneme rules, provided such rules are available for the
-                particular language.
-            </li>
-            <li>
                 <span className='text-green-400'>Green</span> highlighted text has no dictionary look up and
-                has used a machine learning model to generate the transliteration, quality of results will vary. An alternative
-                transliteration created using phoneme rules may be available if the language supports them.
+                has used a machine learning model to generate the transliteration, quality of results will vary. An
+                alternative transliteration created using phoneme rules may be available if the language supports them,
+                otherwise the original word will be an option.
             </li>
         </ul>
     </div>
